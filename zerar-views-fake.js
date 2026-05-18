5ec1a26729102d1084b24dfc50ec5a207ebd3be9
Roberta <beta123soares@gmail.com>
2026-05-18 19:48:07 -0300

    Zera views fake do perfil

---
+++ zerar-views-fake.js
@@ -0,0 +1,8 @@
+﻿const fs = require("fs");
+let s = fs.readFileSync("public/empresa.html","utf8");
+
+s = s.replaceAll("const baseViews = 42800;", "const baseViews = 0;");
+s = s.replaceAll("compact(baseViews)", "compact(0)");
+
+fs.writeFileSync("public/empresa.html",s,"utf8");
+console.log("VIEWS 42.8K REMOVIDO FORCADO.");
