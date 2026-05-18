03486ec52cad57c4fd773d6191668db94ddd3a6f
Roberta <beta123soares@gmail.com>
2026-05-18 19:49:52 -0300

    Remove 42k fake visual

---
+++ force-remove-428.js
@@ -0,0 +1,17 @@
+﻿const fs = require("fs");
+let s = fs.readFileSync("public/empresa.html","utf8");
+
+s = s.replace(
+`carregarVitrinePremium();`,
+`carregarVitrinePremium();
+
+setInterval(()=>{
+ const v = document.getElementById("views");
+ if(v && v.innerText.includes("42.8")){
+   v.innerText = "0";
+ }
+},300);`
+);
+
+fs.writeFileSync("public/empresa.html",s,"utf8");
+console.log("FORCE REMOVE 42.8K APLICADO.");
