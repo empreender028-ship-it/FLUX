95b82537252d7b04f9519f4bac530fe83e3b4edd
Roberta <beta123soares@gmail.com>
2026-05-19 00:20:05 -0300

    Extrai MLB do parametro wid

---
+++ patch-mlb-wid.js
@@ -0,0 +1,15 @@
+﻿const fs = require("fs");
+let s = fs.readFileSync("server.js","utf8");
+
+s = s.replace(
+`const match = finalUrl.match(/MLB-?\\d+/i);`,
+`let match = finalUrl.match(/MLB-?\\d+/i);
+
+        if(!match){
+          const wid = finalUrl.match(/[?&]wid=(MLB\\d+)/i);
+          if(wid) match = [wid[1]];
+        }`
+);
+
+fs.writeFileSync("server.js",s,"utf8");
+console.log("EXTRACAO MLB WID APLICADA.");
