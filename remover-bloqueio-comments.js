96d647a06f5be4e0a018ad24c103efa7f6af6f1a
Roberta <beta123soares@gmail.com>
2026-05-18 21:38:03 -0300

    Remove bloqueio comentarios visitante

---
+++ remover-bloqueio-comments.js
@@ -0,0 +1,12 @@
+﻿const fs = require("fs");
+
+let s = fs.readFileSync("server.js","utf8");
+
+s = s.replace(
+/app\.post\("\/api\/comments",\s*optionalAuth,\s*carregarPlano,\s*verificarRecurso\("podeComentar"\),/g,
+'app.post("/api/comments", optionalAuth,'
+);
+
+fs.writeFileSync("server.js",s,"utf8");
+
+console.log("BLOQUEIO DE COMENTARIO REMOVIDO.");
