4decf6feb89681877553938647defa08b1e14559
Roberta <beta123soares@gmail.com>
2026-05-19 00:01:36 -0300

    Adiciona limpeza de likes reais

---
+++ limpar-likes-reais.js
@@ -0,0 +1,28 @@
+﻿require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI).then(async()=>{
+  const db = mongoose.connection.db;
+  const posts = await db.collection("posts").find({}).toArray();
+
+  let atualizados = 0;
+
+  for(const p of posts){
+    const likedBy = Array.isArray(p.likedBy)
+      ? [...new Set(p.likedBy.filter(Boolean).map(String))]
+      : [];
+
+    await db.collection("posts").updateOne(
+      {_id:p._id},
+      {$set:{
+        likedBy,
+        likes:likedBy.length
+      }}
+    );
+
+    atualizados++;
+  }
+
+  console.log("LIKES REAIS RECALCULADOS:", atualizados);
+  process.exit();
+});
